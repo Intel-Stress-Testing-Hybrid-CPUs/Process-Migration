8785a0cd0485a408dfbeaa21d96cfea154d4a648
ngtaylor <45768742+ngtaylor@users.noreply.github.com>
2021-10-28 18:40:06 -0500

    Update script and add cpp file

---
+++ process-migrate-script_ver_1.ps1
@@ -0,0 +1,21 @@
+#process-migrate-simple.ps1
+#Powershell script for starting a process and changing the running core of the process
+
+
+#replace app.exe with actual application name
+$app_name = "C:\Program Files (x86)\Notepad++\notepad++.exe"
+#$app_arguments = "arg0"
+
+#Create start info for Process Object
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = $app_name
+#$pinfo.Arguments = $app_arguments
+
+#Create Process Object to be run
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+#Start the process
+$p.Start()
+
+#Set processor affinity to 0100
+$p.ProcessorAffinity=0x4
