99387a311423ce2b84a36c7e73048cbe8d42fbc1
JamesRJanecka <jamesjanecka@utexas.edu>
2022-02-28 14:28:30 -0600

    update vtune
    vtune collection continues until the test app exits, so this script cannot kill the app to complete the collection. for a simple test, added an exe which computes the number of primes between 1 and 100,000,000 using OpenMP. each iteration of this takes ~1 sec on my machine, so I have it looping for ~30 seconds, then it finishes and vtune completes. still unable to generate a report due to some error

--- vtune-script.ps1
+++ vtune-script.ps1
@@ -9,14 +9,14 @@ $vtune_path = "C:\Program Files (x86)\Intel\oneAPI\vtune\2022.1.0"
 $vars_path = Join-Path $vtune_path "vtune-vars.bat"
 
 # path of app to test
-$app_path = "C:\Program Files (x86)\Notepad++\notepad++.exe"
+#$app_path = "C:\Program Files (x86)\Notepad++\notepad++.exe"
+$app_path = ".\test_primes.exe"
 
 # command for uarch-exploration
 $vtune_cmd = "vtune -collect uarch-exploration -- `"$app_path`""
 
-# setup environment vars and launch uarch-exploration
-cmd.exe /k "`"$vars_path`"&&$vtune_cmd"
+# command to generate report to file
+$vtune_report = "vtune -report hw-events -r `"..\vtune-results\`""
 
-# kill test app to complete collection
-
-# generate report to file
+# setup environment vars, launch uarch-exploration, generate report
+cmd.exe /k "`"$vars_path`"&&$vtune_cmd&&$vtune_report"
